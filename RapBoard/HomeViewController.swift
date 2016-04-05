c4bb1bebdc0f3b636ab28682648316ba666621ed
Arnold Sandoval <arnoldsandoval@me.com>
2016-04-04 18:54:43 -0700

    comment/clean up NSLog messages

--- RapBoard/HomeViewController.swift
+++ RapBoard/HomeViewController.swift
@@ -12,42 +12,41 @@ import AVFoundation
 
 
 class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, AVAudioPlayerDelegate {
-    
+
     @IBOutlet weak var menuNavButton: UIBarButtonItem!
     @IBOutlet weak var twitterNavButton: UIBarButtonItem!
-    
-    
+
+
     var audioPlayer: AVAudioPlayer = AVAudioPlayer()
-    
+
     func playSound(soundURL: NSURL) {
-        
+
         do { audioPlayer = try AVAudioPlayer(contentsOfURL: soundURL, fileTypeHint: nil) } catch let error as NSError {
             print(error.description)
         }
-        
+
         audioPlayer.numberOfLoops = 0 // play once
         audioPlayer.prepareToPlay()
         audioPlayer.play()
-        
+
         func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
             //You can stop the audio
-            NSLog("done")
-            
+            // NSLog("Audio: Play finished")
         }
-        
+
     }
 
-    
+
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return data.count
     }
-    
+
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         let screenSize: CGRect = UIScreen.mainScreen().bounds
         let screenWidth = screenSize.width
         return CGSize(width: screenWidth/4, height: screenWidth/2.25);
     }
-    
+
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell: SoundboardCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! SoundboardCollectionViewCell
 
@@ -56,20 +55,20 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
 
         cell.rapperName.text = name
         cell.rapperImage.image = UIImage(named: photo)
-        
+
 //        cell.rapperNameTip.transform = CGAffineTransformMakeRotation(-10)
 
         return cell
     }
-    
+
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
 
         let audioFileName = String(data[indexPath.row]["audio"]![0]!)
-        
+
         print(audioFileName)
 
         let audioURL: NSURL = NSBundle.mainBundle().URLForResource(audioFileName, withExtension: "mp3")!
-        
+
         playSound(audioURL)
 
     }
@@ -83,11 +82,11 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     override func viewDidAppear(animated: Bool) {
 
         let nav = self.navigationController?.navigationBar
-        
+
         nav?.barTintColor = UIColor(red:0.668, green:0.085, blue:0.071, alpha:1)
         nav?.tintColor = UIColor(red:0.956, green:0.947, blue:0.866, alpha:1)
 
@@ -101,55 +100,55 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
         titleImageView.image = titleImage
 
         navigationItem.titleView = titleImageView
-        
+
         menuNavButton.title = ""
         menuNavButton.image = UIImage(named: "icon-hamburger")
-        
+
         twitterNavButton.title = ""
         twitterNavButton.image = UIImage(named: "icon-twitter")
-        
+
     }
 
     @IBAction func menuButtonDidTouch(sender: AnyObject) {
         performSegueWithIdentifier("MenuSegue", sender: self)
     }
-    
+
     @IBAction func twitterButtonDidTouch(sender: AnyObject) {
         guard UIApplication.sharedApplication().canOpenURL(NSURL(string: "twitter://user?screen_name=therapboard")!) else {
-                NSLog("No Twitter!!??  You're a better man than I am, Charlie Brown.")
+                NSLog("Twitter: App not installed")
                 performSegueWithIdentifier("WebSegue", sender: self)
                 return
             }
     }
-    
+
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "WebSegue" {
             let toView = segue.destinationViewController as! WebViewController
             let url = "http://www.twitter.com/therapboard"
             toView.url = url
-            
+
             self.minimizeView()
         }
-        
+
         // There is a better way to do this. I just dont know what it is yet.
         if segue.identifier == "MenuSegue" {
             self.minimizeView()
 //            navigationController?.view.transform = CGAffineTransformMakeScale(0.9, 0.9);
-            
+
         }
     }
-    
+
     func minimizeView() {
-        NSLog("minimizeView")
+        // NSLog("minimizeView")
         SpringAnimation.spring(0.7, animations: {
             self.view.transform = CGAffineTransformMakeScale(0.9, 0.9)
         })
     }
-    
+
     func maximizeView(sender: AnyObject) {
         SpringAnimation.spring(0.7, animations: {
             self.view.transform = CGAffineTransformMakeScale(1, 1)
         })
     }
-    
+
 }
